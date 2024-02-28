27c4d246af0297fa615fffe3250830179e948ed9
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-02-29 02:17:32 +0700

    delete 1 file and update 2 files

--- extensions/extensions.sh
+++ extensions/extensions.sh
@@ -1,3 +1,52 @@
 #!/bin/bash
 
 code --list-extensions >extensions/extensions.txt
+
+
+
+
+
+
+
+
+
+# "formulahendry.auto-rename-tag"
+
+# "dracula-theme.theme-dracula"
+# "pkief.material-icon-theme"
+
+# "xabikos.ReactSnippets"
+# "mengrbatinov.json-snippet-to-md-documentation"
+# "blackboxapp.blackbox"
+# "ritwickdey.LiveServer"
+# "ritwickdey.liveserver"
+# "steoates.autoimport"
+# "kamikillerto.vscode-colorize"
+# "streetsidesoftware.code-spell-checker"
+# "ms-vscode.cpptools-themes"
+# "ms-vscode.cpptools-extension-pack"
+# "AMiner.codegeex"
+# "ChakrounAnas.turbo-console-log"
+# "mikestead.dotenv"
+# "humao.rest-client"
+# "aminer.codegeex"
+# "ms-vscode.cpptools"
+# "James-Yu.latex-workshop"
+# "ms-vscode-remote.remote-wsl"
+# "EditorConfig.EditorConfig"
+# "archsense.architecture-view-nestjs"
+# "dbaeumer.vscode-eslint"
+# "james-yu.latex-workshop"
+# "Blackboxapp.blackbox"
+# "xabikos.reactsnippets"
+# "twxs.cmake"
+# "ms-kubernetes-tools.vscode-kubernetes-tools"
+# "Codeium.codeium"
+# "MEngRBatinov.json-snippet-to-md-documentation"
+# "hediet.vscode-drawio"
+# "ms-vscode-remote.remote-containers"
+# "ms-vscode.cmake-tools"
+# "redhat.vscode-yaml"
+# "austenc.tailwind-docs"
+# "alefragnani.bookmarks"
+# "editorconfig.editorconfig"
