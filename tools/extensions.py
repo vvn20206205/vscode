7f62045219d02d07e5aa8e42a3e7b07c82bd3d3e
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-03-06 01:53:06 +0700

    update extensions.json and extensions.py

--- tools/extensions.py
+++ tools/extensions.py
@@ -1,65 +1,79 @@
+# # import subprocess
+
+# # command = "code --list-extensions > ../.vscode/extensions.txt"
+# # # command = "code --list-extensions > extensions.txt"
+
+# # subprocess.run(command, shell=True, check=True)
+
+# # # 
+# # # "formulahendry.auto-rename-tag"
+
+# # # "dracula-theme.theme-dracula"
+# # # "pkief.material-icon-theme"
+
+# # # "xabikos.ReactSnippets"
+# # # "mengrbatinov.json-snippet-to-md-documentation"
+# # # "blackboxapp.blackbox"
+# # # "ritwickdey.LiveServer"
+# # # "ritwickdey.liveserver"
+# # # "steoates.autoimport"
+# # # "kamikillerto.vscode-colorize"
+# # # "ms-vscode.cpptools-themes"
+# # # "ms-vscode.cpptools-extension-pack"
+# # # "AMiner.codegeex"
+# # # "ChakrounAnas.turbo-console-log"
+# # # "mikestead.dotenv"
+# # # "humao.rest-client"
+# # # "aminer.codegeex"
+# # # "ms-vscode.cpptools"
+# # # "ms-vscode-remote.remote-wsl"
+# # # "EditorConfig.EditorConfig"
+# # # "archsense.architecture-view-nestjs"
+# # # "dbaeumer.vscode-eslint"
+# # # "Blackboxapp.blackbox"
+# # # "xabikos.reactsnippets"
+# # # "twxs.cmake"
+# # # "ms-kubernetes-tools.vscode-kubernetes-tools"
+# # # "Codeium.codeium"
+# # # "MEngRBatinov.json-snippet-to-md-documentation"
+# # # "hediet.vscode-drawio"
+# # # "ms-vscode-remote.remote-containers"
+# # # "ms-vscode.cmake-tools"
+# # # "redhat.vscode-yaml"
+# # # "austenc.tailwind-docs"
+# # # "alefragnani.bookmarks"
+# # # "editorconfig.editorconfig"
 # import subprocess
 
-# command = "code --list-extensions > ../.vscode/extensions.txt"
-# # command = "code --list-extensions > extensions.txt"
+# command = "code --list-extensions"
+    
+# extensions = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True).stdout.splitlines()
 
-# subprocess.run(command, shell=True, check=True)
-
-# # 
-# # "formulahendry.auto-rename-tag"
-
-# # "dracula-theme.theme-dracula"
-# # "pkief.material-icon-theme"
-
-# # "xabikos.ReactSnippets"
-# # "mengrbatinov.json-snippet-to-md-documentation"
-# # "blackboxapp.blackbox"
-# # "ritwickdey.LiveServer"
-# # "ritwickdey.liveserver"
-# # "steoates.autoimport"
-# # "kamikillerto.vscode-colorize"
-# # "ms-vscode.cpptools-themes"
-# # "ms-vscode.cpptools-extension-pack"
-# # "AMiner.codegeex"
-# # "ChakrounAnas.turbo-console-log"
-# # "mikestead.dotenv"
-# # "humao.rest-client"
-# # "aminer.codegeex"
-# # "ms-vscode.cpptools"
-# # "ms-vscode-remote.remote-wsl"
-# # "EditorConfig.EditorConfig"
-# # "archsense.architecture-view-nestjs"
-# # "dbaeumer.vscode-eslint"
-# # "Blackboxapp.blackbox"
-# # "xabikos.reactsnippets"
-# # "twxs.cmake"
-# # "ms-kubernetes-tools.vscode-kubernetes-tools"
-# # "Codeium.codeium"
-# # "MEngRBatinov.json-snippet-to-md-documentation"
-# # "hediet.vscode-drawio"
-# # "ms-vscode-remote.remote-containers"
-# # "ms-vscode.cmake-tools"
-# # "redhat.vscode-yaml"
-# # "austenc.tailwind-docs"
-# # "alefragnani.bookmarks"
-# # "editorconfig.editorconfig"
+# content = {
+#     "recommendations": extensions
+# }
+    
+# # sửa '' trong content thành ""
+# content=content.
+# print("🚀 Giá trị của content:", content)
+# # with open('../.vscode/extensions.json', 'w') as file:
+# #     file.write(str(content))
 import subprocess
+import json
 
-def main():
-    # Command to get the list of installed extensions in VSCode
-    command = "code --list-extensions"
-    
-    # Thực hiện câu lệnh và lưu kết quả vào biến extensions
-    extensions = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True).stdout.splitlines()
-    
-    # Tạo nội dung cho tệp extensions.json
-    content = {
-        "recommendations": extensions
-    }
-    
-    # Ghi nội dung vào tệp extensions.json
-    with open('../.vscode/extensions.json', 'w') as file:
-        file.write(str(content))
+# Define the command to list extensions
+command = "code --list-extensions"
 
-if __name__ == "__main__":
-    main()
+# Run the command and capture the output
+extensions = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True).stdout.splitlines()
+
+# Create content dictionary
+content = {
+    "recommendations": extensions
+}
+
+# Write the content to a JSON file
+with open("recommended_extensions.json", "w") as json_file:
+    json.dump(content, json_file, indent=4)
+
+print("Recommended extensions saved to recommended_extensions.json")
