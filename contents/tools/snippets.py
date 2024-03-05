88ca545f5a06403903f47a21e24706b4f5b36661
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-03-06 02:08:50 +0700

    update snippets.py

--- contents/tools/snippets.py
+++ contents/tools/snippets.py
@@ -1,16 +1,17 @@
 import os
 import shutil
 
+
+def get_snippets_path():
+    if os.name == 'nt':
+        return os.path.expanduser('~/AppData/Roaming/Code/User/snippets')
+    else:
+        return os.path.expanduser('~/.config/Code/User/snippets')
+
+
 source_folder = "../snippets"
-destination_folder = "c:/Users/vvn20206205/AppData/Roaming/Code/User/snippets"
-# destination_folder = "/home/<tên người dùng>/.config/Code/User/snippets"
+destination_folder = get_snippets_path()
 
-# if not os.path.exists(source_folder):
-#     print(f"Thư mục nguồn '{source_folder}' không tồn tại.")
-#     exit()
-
-# if not os.path.exists(destination_folder):
-#     os.makedirs(destination_folder)
 
 for root, dirs, files in os.walk(source_folder):
     for file in files:
